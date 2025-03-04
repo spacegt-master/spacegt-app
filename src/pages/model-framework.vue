3afe359d6816eeec43da264b73a7a51d59a16fe6
WANG <541980655@qq.com>
2025-03-04 09:30:15 +0800

    优化模型框架布局

--- src/pages/model-framework.vue
+++ src/pages/model-framework.vue
@@ -14,63 +14,65 @@
     </app-bar>
     <v-main class="100%">
       <v-container class="pa-6 h-100" fluid>
-        <v-row class="h-100">
-          <v-col cols="12" md="8" order="2" order-md="1" class="h-100 ">
+        <v-row class="h-100 overflow-auto">
+          <v-col class="h-100" cols="12" md="8" order="2" order-md="1">
             <!-- <v-img color="surface-light" height="100%" rounded="lg" /> -->
             <model-render class="rounded-lg	"></model-render>
           </v-col>
+          <v-col class="pl-md-4" :class="{ 'h-100': $vuetify.display.mdAndUp }" cols="12" md="4" order="1" order-md="2">
+            <v-sheet class=" h-100 pr-4" :class="{ 'overflow-auto': $vuetify.display.mdAndUp }">
+              <v-rating color="amber-darken-2" density="compact" model-value="4" readonly />
 
-          <v-col class="px-md-4 h-100 overflow-auto" cols="12" md="4" order="1" order-md="2">
-            <v-rating color="amber-darken-2" density="compact" model-value="4" readonly />
+              <div class="text-md-h4 font-weight-bold mb-2 text-h5">Developer Backpack</div>
 
-            <div class="text-md-h4 font-weight-bold mb-2 text-h5">Developer Backpack</div>
+              <p class="text-caption font-weight-bold mb-4 text-medium-emphasis">Version 1.0 (Updated June 5, 2021)</p>
 
-            <p class="text-caption font-weight-bold mb-4 text-medium-emphasis">Version 1.0 (Updated June 5, 2021)</p>
+              <div class="text-body-1 mb-4 text-medium-emphasis">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+                ratione debitis quis est labore voluptatibus! Eaque cupiditate minima,
+                at placeat totam, magni doloremque veniam neque porro libero rerum
+                unde voluptatem!
+              </div>
 
-            <div class="text-body-1 mb-4 text-medium-emphasis">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-              ratione debitis quis est labore voluptatibus! Eaque cupiditate minima,
-              at placeat totam, magni doloremque veniam neque porro libero rerum
-              unde voluptatem!
-            </div>
+              <v-row>
+                <v-col cols="6" lg="6" md="12">
+                  <v-btn block class="text-none" color="primary" flat rounded="lg" size="large" text="Download" />
+                </v-col>
 
-            <v-row>
-              <v-col cols="6" lg="6" md="12">
-                <v-btn block class="text-none" color="primary" flat rounded="lg" size="large" text="Download" />
-              </v-col>
+                <v-col cols="6" lg="6" md="12">
+                  <v-btn block class="text-none" color="primary" flat rounded="lg" size="large" text="Preview"
+                    variant="tonal" />
+                </v-col>
+              </v-row>
 
-              <v-col cols="6" lg="6" md="12">
-                <v-btn block class="text-none" color="primary" flat rounded="lg" size="large" text="Preview"
-                  variant="tonal" />
-              </v-col>
-            </v-row>
+              <v-divider class="my-6" />
 
-            <v-divider class="my-6" />
+              <p class="text-body-1 font-weight-bold mt-4 mb-2">Highlights</p>
 
-            <p class="text-body-1 font-weight-bold mt-4 mb-2">Highlights</p>
+              <ul class="ps-5">
+                <li v-for="feature in features" :key="feature" class="pa-0" density prepend-icon="mdi-circle-small">
+                  <span class="text-body-2">{{ feature }}</span>
+                </li>
+              </ul>
 
-            <ul class="ps-5">
-              <li v-for="feature in features" :key="feature" class="pa-0" density prepend-icon="mdi-circle-small">
-                <span class="text-body-2">{{ feature }}</span>
-              </li>
-            </ul>
+              <v-divider class="my-6" />
 
-            <v-divider class="my-6" />
+              <p class="text-body-1 font-weight-bold mt-4 mb-2">Details</p>
 
-            <p class="text-body-1 font-weight-bold mt-4 mb-2">Details</p>
+              <p class="text-body-2 mb-4 text-medium-emphasis">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore
+                voluptatibus!
+                Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde
+                voluptatem!
+              </p>
 
-            <p class="text-body-2 mb-4 text-medium-emphasis">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore
-              voluptatibus!
-              Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde
-              voluptatem!
-            </p>
+              <v-divider class="my-6" />
 
-            <v-divider class="my-6" />
+              <p class="text-body-1 font-weight-bold mt-4 mb-2">Share</p>
 
-            <p class="text-body-1 font-weight-bold mt-4 mb-2">Share</p>
-
-            <v-btn v-for="icon in icons" :key="icon" color="medium-emphasis" :icon="icon" size="small" variant="text" />
+              <v-btn v-for="icon in icons" :key="icon" color="medium-emphasis" :icon="icon" size="small"
+                variant="text" />
+            </v-sheet>
           </v-col>
         </v-row>
       </v-container>
