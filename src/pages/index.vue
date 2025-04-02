1ea65f5022824a85b0719c83d43909de72752d0f
WANG <541980655@qq.com>
2025-04-02 18:58:09 +0800

    认证过期自动清除用户信息

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,31 @@
   <layout></layout>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 import Layout from '@/layouts/default.vue'
+import { useAuthorizationStore } from '../stores/authorization'
+import { TokenApi } from '@/api/accounts/token'
+import { onMounted, nextTick } from 'vue'
+import { useAccountsStore } from '@/stores/data/accounts'
+
+const authorizationStore = useAuthorizationStore()
+const accountsStore = useAccountsStore()
+
+onMounted(async () => {
+  if (authorizationStore.token) {
+    const res = await TokenApi.validate(authorizationStore.token)
+
+    if (!res) {
+      accountsStore.account = null
+      accountsStore.authorities = []
+
+      authorizationStore.token = ''
+    }
+  } else {
+    accountsStore.account = null
+    accountsStore.authorities = []
+
+    authorizationStore.token = ''
+  }
+}) 
 </script>
